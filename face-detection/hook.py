5716a5380a84cf4ff7f5a186e25bad0debf2bd3c
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2019-01-28 16:23:58 +0400

    Add table output and thumbnails

--- face-detection/hook.py
+++ face-detection/hook.py
@@ -1,4 +1,6 @@
+import base64
 import io
+import json
 import logging
 
 import numpy as np
@@ -71,6 +73,76 @@ def add_overlays(frame, boxes, labels=None):
                 )
 
 
+def image_resize(image, width=None, height=None, inter=Image.ANTIALIAS):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.height, image.width
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = image.resize(dim, inter)
+
+    # return the resized image
+    return resized
+
+
+def result_table_string(result_dict, ctx):
+    table = []
+
+    def crop_from_box(box, normalized_coordinates=True):
+        left, right = box[0], box[2]
+        top, bottom = box[1], box[3]
+        if normalized_coordinates:
+            left, right = left * ctx.image.width, right * ctx.image.width
+            top, bottom = top * ctx.image.height, bottom * ctx.image.height
+
+        cropped = ctx.image.crop((left, top, right, bottom))
+        cropped = image_resize(cropped, width=256)
+        image_bytes = io.BytesIO()
+        cropped.convert('RGB').save(image_bytes, format='JPEG', quality=80)
+
+        return image_bytes.getvalue()
+
+    def append(type_, name, prob, image):
+        encoded = image
+        if image is not None:
+            encoded = base64.encodebytes(image).decode()
+
+        table.append(
+            {
+                'type': type_,
+                'name': name,
+                'prob': float(prob),
+                'image': encoded
+            }
+        )
+
+    if len(result_dict.get('face_boxes', [])) > 0:
+        for prob, box in zip(result_dict['face_scores'], result_dict['face_boxes']):
+            append('face', 'face', prob, crop_from_box(box))
+
+    return json.dumps(table)
+
+
 def postprocess(outputs, ctx):
     outputs = list(outputs.values())[0].reshape([-1, 7])
     # 7 values:
@@ -90,15 +162,20 @@ def postprocess(outputs, ctx):
             bboxes[i][3] = box[6] * h
             labels.append('%.3f. %s' % (box[2], box[1]))
 
+        result = {'face_boxes': bboxes_raw[:, 3:7], 'face_scores': bboxes_raw[:, 2]}
+        table = result_table_string(result, ctx)
         add_overlays(ctx.image, bboxes, labels)
+    else:
+        table = []
 
     image_bytes = io.BytesIO()
 
     im = ctx.image
-    im.save(image_bytes, format='PNG')
+    im.save(image_bytes, format='JPEG', quality=80)
 
     return {
         'output': image_bytes.getvalue(),
+        'table_output': table
         # 'boxes': ctx.bounding_boxes,
         # 'labels': np.array(labels)
     }
